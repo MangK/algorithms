903e70206e0137f02a389be72a62974432c4e613
mangk <mangk.xyz@gmail.com>
2021-07-09 19:06:35 +0800

    sever model

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module DragFile
 
 go 1.16
 
-require github.com/atotto/clipboard v0.1.4 // indirect
+require (
+	github.com/atotto/clipboard v0.1.4 // indirect
+	github.com/chai2010/winsvc v0.0.0-20200705094454-db7ec320025c // indirect
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+)
--- main.go
+++ main.go
@@ -5,12 +5,45 @@ import (
 	"DragFile/params"
 	server_http "DragFile/server/http"
 	"fmt"
+	"github.com/chai2010/winsvc"
+	"log"
+	"os"
+	"path/filepath"
 )
 
+var (
+	appPath string // 程序的绝对路径
+)
+func init() {
+	var err error
+	if appPath, err = winsvc.GetAppPath(); err != nil {
+		log.Fatal(err)
+	}
+	if err := os.Chdir(filepath.Dir(appPath)); err != nil {
+		log.Fatal(err)
+	}
+}
+
 func main() {
+	if !winsvc.IsAnInteractiveSession() {
+		log.Println("main:", "runService")
+		if err := winsvc.RunAsService("AAAAAAAA", start, stop, false); err != nil {
+			log.Fatalf("svc.Run: %v\n", err)
+		}
+		return
+	}
+
+	start()
+}
+
+func start() {
 	param := params.GetParams()
 	defer myFile.HasFileOrDropDir(param.Dir)
 
 	fmt.Println("Current Config:", param)
 	server_http.Start(param)
+}
+
+func stop() {
+
 }
